152330b0de8b18d59793fa48ff5fbd82ff4e4a73
SH <407steven.h@gmail.com>
2025-08-08 02:38:08 -0500

    contact form test

--- js/script.js
+++ js/script.js
@@ -132,4 +132,21 @@ jQuery(function ($) {
 		}
 		pageSlider();
 	});
+
+	$("#contact-form").submit(function (e) {
+		e.preventDefault();
+
+		$.ajax({
+			method: "POST",
+			url: "https://formsubmit.co/ajax/1fecbb73600da2d87b371562a8fde953",
+			dataType: "json",
+			accepts: "application/json",
+			data: {
+				name: "FormSubmit",
+				message: "I'm from Devro LABS",
+			},
+			success: (data) => console.log(data),
+			error: (err) => console.log(err),
+		});
+	});
 });
